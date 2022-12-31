0dfcd2acb884b08a35d8a15b1e273768ed27fbbf
BastionAtackDev <sbypsm@gmail.com>
2022-12-31 16:49:59 +0200

    Creating Shell-Fireworks.sh

---
+++ shell-fireworks.sh
@@ -0,0 +1,245 @@
+#!/usr/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo  -e "${RED}            /";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo -e "${YELLOW}            0";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "          ";
+echo "           \|/";
+echo -e "          --${RED}0${YELLOW}--";
+echo "           /|\\";
+echo "          ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo -e "          ${YELLOW}\ | /";
+echo -e "           ${RED}\|/";
+echo -e "        ${YELLOW}--${RED}--0--${YELLOW}--";
+echo -e "           ${RED}/|\\";
+echo -e "          ${YELLOW}/ | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo -e "         ${RED} \ | /";
+echo "           \|/";
+echo "        ---   ---";
+echo "           /|\\";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "          \ | /";
+echo "             ";
+echo "        --     --";
+echo "              ";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
+echo "";
+echo "";
+echo "          \ | /";
+echo "           ";
+echo "        -       -";
+echo "           ";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+# sleep 1
+clear
