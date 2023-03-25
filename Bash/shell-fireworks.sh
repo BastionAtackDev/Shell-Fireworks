55edcc3e517f9c78466f09f00ddcb58c642af98f
BastionAtackDev <88817320+BastionAtackDev@users.noreply.github.com>
2023-03-25 18:09:18 +0200

    Add files via upload

---
+++ Bash/shell-fireworks.sh
@@ -0,0 +1,246 @@
+#!/usr/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+DELAY=0.001
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo  -e "${RED}            /";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo "";
+echo "            /";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "";
+echo "";
+echo -e "${YELLOW}            0";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "          ";
+echo "           \|/";
+echo -e "          --${RED}0${YELLOW}--";
+echo "           /|\\";
+echo "          ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo -e "          ${YELLOW}\ | /";
+echo -e "           ${RED}\|/";
+echo -e "        ${YELLOW}--${RED}--0--${YELLOW}--";
+echo -e "           ${RED}/|\\";
+echo -e "          ${YELLOW}/ | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo -e "         ${RED} \ | /";
+echo "           \|/";
+echo "        ---   ---";
+echo "           /|\\";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "          \ | /";
+echo "             ";
+echo "        --     --";
+echo "              ";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
+echo "";
+echo "";
+echo "          \ | /";
+echo "           ";
+echo "        -       -";
+echo "           ";
+echo "          / | \\";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+echo "            ";
+sleep $DELAY
+clear
